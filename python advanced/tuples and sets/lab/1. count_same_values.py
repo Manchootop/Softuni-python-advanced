8a6d29dd30d6ed1b26719ad076e0191b675eadcb
Mariyan Georgiev <mariqn5000@gmail.com>
2023-11-15 23:52:14 +0200

    tuples_and_sets

---
+++ python advanced/tuples and sets/lab/1. count_same_values.py
@@ -0,0 +1,11 @@
+count = int(input())
+students = {}
+for _ in range(count):
+    line = tuple(input().split())
+    student, grade = line
+    if student not in students:
+        students[student] = []
+    students[student].append(float(grade))
+
+[print(student) for student in students]
+
